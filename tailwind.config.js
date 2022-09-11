c3e9061971b9de221fd02471080a7ff1bf079a8b
reza <rezamkamali79@gmail.com>
2022-09-11 10:38:33 +0430

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,11 @@ module.exports = {
         veryDarkBlue: "hsl(233, 12%, 13%)",
         veryPaleRed: "hsl(13, 100%, 96%)",
         veryLightGray: "hsl(0, 0%, 98%)",
+        primary: "#FF6363",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
       },
     },
   },
