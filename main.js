7f4a7dc326b2c71fbef75b38fd7bd05ac2ded266
reza <rezamkamali79@gmail.com>
2022-09-13 21:39:40 +0430

    function

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
-btn.addEventListener("click", () => {
-    btn.classList.toggle("open")
-    nav.classList.toggle("flex");
-    nav.classList.toggle("hidden");
-})
+btn.addEventListener("click",clickButton)
+
+function clickButton () {
+ btn.classList.toggle("open");
+ nav.classList.toggle("flex");
+ nav.classList.toggle("hidden");
+}
